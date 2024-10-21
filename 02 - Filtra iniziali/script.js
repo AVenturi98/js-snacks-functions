ca21fd61b61c65c5c10e800d97d0b5ec2e6fafbc
AVenturi98 <lssndrventuri@gmail.com>
2024-10-21 17:00:26 +0200

    ADD: ex.2

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,26 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const namesFilter = [];
 
 // Invoca la funzione qui e stampa il risultato in console
 
+function filterName(n) {
+    const firstLetterSearch = 'A';
+
+    for (let i = 0; i < n.length; i++) {
+        
+        const name = n[i]
+
+        const letterFind = name[0]
+
+        if (letterFind ===firstLetterSearch) {
+            namesFilter.push(name)
+        }
+    }
+}
+filterName(names)
+console.log(namesFilter)
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
