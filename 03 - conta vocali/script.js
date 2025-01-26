cf2e5bee2b8447e82a5104c1c0c6054bb4276ff3
AVenturi98 <lssndrventuri@gmail.com>
2025-01-26 20:37:33 +0100

    EDIT ex. 03 "conta vocali"

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -6,8 +6,24 @@ const word = 'javascript';
 // Dichiara la funzione qui.
 
 
+const newArray = []
+
 // Invoca la funzione qui e stampa il risultato in console
 
+function countVocal() {
+    const vocalLetter = ['a', 'e', 'i', 'o', 'u']
+
+    for (let i = 0; i < word.length; i++) {
+        const element = word[i];
+        const element_Included = vocalLetter.includes(element)
+        if (element_Included == true) {
+            newArray.push(element_Included)
+            console.log(element_Included, element, newArray.length)
+        }
+    }
+    console.log(newArray.length)
+}
+countVocal()
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
