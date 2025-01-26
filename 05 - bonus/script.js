e4f5ca21292ce28788f943bf2dfa374e602ca2df
AVenturi98 <lssndrventuri@gmail.com>
2025-01-26 21:02:39 +0100

    EDIT ex. 05 "bonus"

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,26 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const nome = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+const current = new Date()
+const hour = current.getHours()
+const morning = 'Buon giorno'
+const afternoon = 'Buon pomeriggio'
+const night = 'Buona sera'
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+function hello(n, person) {
+    return console.log(n, person)
+}
+if (hour < 13) hello(morning, nome)
+if (hour > 13 && hour < 17) hello(afternoon, nome)
+if (hour > 17) hello(night, nome)
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
